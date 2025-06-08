9be94797501c89f6b5ea6c87fa512d6bcc274304
Antorou <rousselotantoine7@gmail.com>
2025-06-08 18:14:14 +0200

    added datepckr, and few changes on dashborad

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+
   def dashboard
     @rooms = current_user.rooms
-    @bookings = current_user.bookings.where('start_date >= ?', Date.today).order(:start_date)
+    
+    @current_bookings = current_user.bookings
+      .where('start_date <= ? AND end_date >= ?', Date.today, Date.today)
+      .order(:start_date)
+    
+    @upcoming_bookings = current_user.bookings
+      .where('start_date > ?', Date.today)
+      .order(:start_date)
+    
+    @past_bookings = current_user.bookings
+      .where('end_date < ?', Date.today)
+      .order(start_date: :desc)
   end
+
+
 end
--- app/controllers/users_controllers.rb
+++
@@ -1,4 +0,0 @@
-  def dashboard
-    @my_rooms = current_user.rooms
-    @my_bookings = current_user.bookings.includes(:room).order(start_date: :asc)
-  end
