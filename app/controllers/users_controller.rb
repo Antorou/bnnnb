e7a3f8564768f6464fe31a2bd53a17619ebbe729
Antorou <rousselotantoine7@gmail.com>
2025-06-08 18:29:19 +0200

    tuboooo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,26 @@ class UsersController < ApplicationController
 
 
   def dashboard
-    @rooms = current_user.rooms
-    
-    @current_bookings = current_user.bookings
-      .where('start_date <= ? AND end_date >= ?', Date.today, Date.today)
-      .order(:start_date)
-    
-    @upcoming_bookings = current_user.bookings
-      .where('start_date > ?', Date.today)
-      .order(:start_date)
-    
-    @past_bookings = current_user.bookings
-      .where('end_date < ?', Date.today)
-      .order(start_date: :desc)
+  @rooms = current_user.rooms
+
+  @current_bookings = current_user.bookings
+    .where('start_date <= ? AND end_date >= ?', Date.today, Date.today)
+    .order(:start_date)
+
+  @upcoming_bookings = current_user.bookings
+    .where('start_date > ?', Date.today)
+    .order(:start_date)
+
+  @past_bookings = current_user.bookings
+    .where('end_date < ?', Date.today)
+    .order(start_date: :desc)
+
+  respond_to do |format|
+    format.html # initial page
+    format.turbo_stream { render partial: "bookings", locals: { current_bookings: @current_bookings, upcoming_bookings: @upcoming_bookings, past_bookings: @past_bookings, tab: params[:tab] } }
   end
+end
+
 
 
 end
